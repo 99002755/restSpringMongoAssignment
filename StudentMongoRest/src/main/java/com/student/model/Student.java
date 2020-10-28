de769b227e4dee6428c483ee050350bae2da99e1
99002755 <72496610+99002755@users.noreply.github.com>
2020-10-28 09:44:44 +0530

    Update Student.java

--- StudentMongoRest/src/main/java/com/student/model/Student.java
+++ StudentMongoRest/src/main/java/com/student/model/Student.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="studentmongo")
+@Document(collection="studentMongo")
 @TypeAlias("Student")
 public class Student {
 	@Id
